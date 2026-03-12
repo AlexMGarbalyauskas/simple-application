a122422df9aff098546d3feb88bfd645d725915e
Alex <alexgarbalyauskas@gmail.com>
2026-03-12 19:15:13 +0000

    Fix 502: use correct node entry point bin/www and wait for app ready

--- start.sh
+++ start.sh
@@ -2,7 +2,13 @@
 set -e
 
 # Start Node app in background
-node /app/app.js &
+cd /app && node ./bin/www &
+
+# Wait for Node to be ready on port 3000
+for i in $(seq 1 10); do
+  nc -z 127.0.0.1 3000 && break
+  sleep 1
+done
 
 # Start nginx in foreground
 nginx -g "daemon off;"
